e81b1276321543b429e5841107c63b720e3e81ad
Johnny <ENDARAJC@innovasi.com>
2023-04-28 02:44:05 -0700

    automation 1

---
+++ cypress.config.js
@@ -0,0 +1,40 @@
+
+const { defineConfig } = require("cypress")
+
+module.exports = defineConfig({
+  retries:{
+    runMode: 1
+  },
+  pageLoadTimeout: 200000,
+  defaultCommandTimeout: 200000,
+  projectId: '1huejn',
+
+  e2e: {setupNodeEvents(on, config) {
+
+    on("before:browser:launch", (browser = {}, launchOptions) => {
+      
+    });
+  
+    on("task", {
+      
+    });
+    
+    }, 
+    
+    baseUrl:'https://tsharpslot1qa.devsd.innovasi.com/',
+    //baseUrl:'https://sharpproductionmirror.devsd.innovasi.com/',
+  
+   specPattern:'cypress/e2e/RegressionTests/*.cy.js',
+  // specPattern:'cypress/e2e/Temp/1-getting-started/*.js'
+ 
+  },
+
+  experimentalStudio:true,
+  watchForFileChanges:true,
+  reporter: "junit",
+  reporterOptions:{
+   "mochaFile":"results/my-test-output-[hash].xml"
+  }
+ 
+})
+
---
+++ cypress/support/commands.js
@@ -0,0 +1,51 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+
+Cypress.Commands.add('login1', (username) =>  {
+    
+    cy.log(username + " (AppAdm)")
+
+    cy.get('[title="Accept Notice"]').click()
+    cy.get('#username')
+        .type(username)
+    cy.get('.ui-menu-item')
+        .click()
+    cy.get('#signin')
+        .click()
+
+        // // IF THERE IS A POPUP
+        // cy.window().then(function(){
+
+        //     if('#SummaryOfChangesHeaderButtonId').exists(){
+        //     cy.get('#SummaryOfChangesHeaderButtonId').click()
+
+        //  });
+
+    cy.get('a:contains("Main")').should('be.visible')
+
+} )
